30cd7ff9e63ed47c0b8846a6cd326d7af4f20ee5
Johnnys Martins <Johnnys Martins>
2016-07-28 01:28:55 -0300

    arrumando a busca.

--- js/main.js
+++ js/main.js
@@ -13,3 +13,14 @@ function limpa(){
         document.getElementById("busca").value='';
     }
 }
+$(function busca(){
+			$("#txtBusca").keyup(function(){
+				var texto = $(this).val();
+		
+				$("#ulItens li").css("display", "block");
+				$("#ulItens li").each(function(){
+					if($(this).text().toUpperCase().indexOf(texto.toUpperCase()) < 0)
+   						$(this).css("display", "none");
+				});
+			});
+		});
